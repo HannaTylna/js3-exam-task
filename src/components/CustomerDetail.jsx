12396dab073d244ed9c48a000d3fe4dba903fdfb
Hanna <hanna.tylna@gmail.com>
2021-12-22 13:17:57 +0100

    add button delete

--- src/components/CustomerDetail.jsx
+++ src/components/CustomerDetail.jsx
@@ -1,24 +1,46 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function CustomerDetail(props) {
    const [customerDetail, setCustomerDetail] = useState({});
+   const navigate = useNavigate();
 
    useEffect(() => {
-      const url = `https://frebi.willandskill.eu/api/v1/customers/${props.id}/`;
-      const token = localStorage.getItem("exam");
-      fetch(url, {
-         method: "GET",
-         headers:{
-            "Content-Type":"application/json",
-            "Authorization":`Bearer ${token}`
-         }
-      })
+      function fetchData() {
+         const url = `https://frebi.willandskill.eu/api/v1/customers/${props.id}/`;
+         const token = localStorage.getItem("exam");
+         fetch(url, {
+            method: "GET",
+            headers:{
+               "Content-Type":"application/json",
+               "Authorization":`Bearer ${token}`
+            }
+         })
          .then(res => res.json())
          .then(data => {
-            console.log(data)
-            setCustomerDetail(data)
+            console.log(data);
+            setCustomerDetail(data);
          })
+      }
+      fetchData();
    }, [props.id])
+
+
+   function handleOnDelete(id) {
+      console.log(id);
+      const url = `https://frebi.willandskill.eu/api/v1/customers/${props.id}/`;
+      const token = localStorage.getItem("exam");
+      const headers = {
+         "Content-Type": "application/json",
+         "Authorization": `Bearer ${token}`
+      };
+      fetch(url, {
+         method: "DELETE",
+         headers: headers
+      })
+      .then((res) => navigate("/home"))
+   }
+
    return (
       <div>
          {customerDetail ? (
@@ -32,7 +54,8 @@ export default function CustomerDetail(props) {
                <p><strong>Website:</strong> {customerDetail.website}</p>
                <p><strong>Email:</strong> {customerDetail.email}</p>
                <p><strong>Phone number:</strong> {customerDetail.phoneNumber}</p>
-               
+               <button type='submit'>Update information</button>
+               <button onClick={(e) => handleOnDelete(customerDetail.id)}>Delete customer</button>
             </>
          ) : "Not found"}
       </div>
